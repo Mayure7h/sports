653755a66eaae633ed8a6a6cae6466b592286cb5
mayuresh <mayureshnaik2003@gmail.com>
2025-03-05 00:49:42 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db.js');
-const productRoutes = require('./routes/productRoutes.js');
-const salesRoutes = require('./routes/salesRoutes.js');
-const purchaseRoutes = require('./routes/purchaseRoutes.js');
+// const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import salesRoutes from './routes/salesRoutes.js';
+import purchaseRoutes from './routes/purchaseRoutes.js';
+
+import path from 'path';
 
 const app = express();
 const PORT = 5000;
@@ -12,14 +15,22 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+
 // Database connection
 connectDB();
+
+const _dirname = path.resolve();
+
 
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/sales', salesRoutes);
 app.use('/api/purchases', purchaseRoutes);
+// app.get('/', (req, res) => res.send('🏃‍♂ Sports Store API Running!'));
 
-app.get('/', (req, res) => res.send('🏃‍♂ Sports Store API Running!'));
+app.use(express.static(path.join(_dirname , "/frontend/build")));
+app.get("*", (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+})
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
